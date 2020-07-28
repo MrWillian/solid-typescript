2de6e6c61324a058a1bf18ba1e96d3fe746a3d53
MrWilllian <willian.dev10@gmail.com>
2020-07-27 23:14:06 -0300

    refactor: fix error argument name

--- analytics/src/infra/sentry-adapter.ts
+++ analytics/src/infra/sentry-adapter.ts
@@ -1,5 +1,5 @@
 import { ErrorAnalytics } from "../analytics/error-analytics";
 
 export class SentryAdapter implements ErrorAnalytics {
-  saveError(errir: any): void {}
+  saveError(error: any): void {}
 }
