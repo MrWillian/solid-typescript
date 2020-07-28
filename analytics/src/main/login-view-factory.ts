b5afe8252292033e2c47a287350d975ccd05a33b
MrWilllian <willian.dev10@gmail.com>
2020-07-27 23:14:59 -0300

    refactor: add login view factory with aall dependency inversion

--- analytics/src/main/login-view-factory.ts
+++ analytics/src/main/login-view-factory.ts
@@ -4,12 +4,18 @@ import { ErrorLog } from '../analytics/error-log';
 import { ScreenLog } from '../analytics/screen-log';
 import { ActionLog } from '../analytics/action-log';
 import { SentryAdapter } from '../infra/sentry-adapter';
+import { LogglyAdapter } from '../infra/loggly-adapter';
 import { FirebaseAdapter } from '../infra/firebase-adapter';
+import { ErrorAnalyticsComposite } from '../infra/error-analytics-composite';
 
 export const makeLoginView = (): LoginView => {
-  const sentryAdapter = new SentryAdapter();
+  const errorAnalyticsComposite = new ErrorAnalyticsComposite([
+    new SentryAdapter(),
+    new LogglyAdapter()
+  ]);
+
   const firebaseAdapter = new FirebaseAdapter();
-  const errorLog = new ErrorLog(sentryAdapter);
+  const errorLog = new ErrorLog(errorAnalyticsComposite);
   const actionLog = new ActionLog(firebaseAdapter);
   const screenLog = new ScreenLog(firebaseAdapter);
 
