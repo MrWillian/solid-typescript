aa24791757de02c9e72cb736718d6454e7991776
MrWilllian <willian.dev10@gmail.com>
2020-07-27 23:13:57 -0300

    refactor: fix error argument name

--- analytics/src/infra/loggly-adapter.ts
+++ analytics/src/infra/loggly-adapter.ts
@@ -1,5 +1,5 @@
 import { ErrorAnalytics } from "../analytics/error-analytics";
 
 export class LogglyAdapter implements ErrorAnalytics {
-  saveError(errir: any): void {}
+  saveError(error: any): void {}
 }
